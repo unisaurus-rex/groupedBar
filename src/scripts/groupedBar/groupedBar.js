550681a68c04e2628223b31589faf7a5a2706592
U-AD001\U315546 <U315546@L76BT3G72.AD001.InfoFTPS.COM>
2016-12-15 14:46:12 -0700

    got rid of some unnecessary code

--- src/scripts/groupedBar/groupedBar.js
+++ src/scripts/groupedBar/groupedBar.js
@@ -9,19 +9,17 @@ export default function groupedBarChart(){
     height = 250 - margin.top - margin.bottom;
 
   //create scales
-  /*var x0 = d3.scale.ordinal()
-    .rangeRoundBands([0, width], .1);*/
-    var x0 = d3.scaleBand()
+  var x0 = d3.scaleBand()
     .rangeRound([0, width])
-    ;
+  ;
 
+  var x1 = d3.scaleBand()
+    .paddingOuter(1)
+  ; 
 
-  //var x1 = d3.scale.ordinal();
-  var x1 = d3.scaleBand(); 
   var y = d3.scaleLinear()
     .range([height, 0])
-    ;
-
+  ;
 
   //colors
   var color = d3.scaleOrdinal()
@@ -30,10 +28,10 @@ export default function groupedBarChart(){
   //x axis
   var xAxis = d3.axisBottom()
     .scale(x0)
-    //.tickSizeInner(-height)
-    //.tickSizeOuter(0)
-    //.tickPadding(10)
-    ;
+    .tickSizeInner(-height)
+    .tickSizeOuter(0)
+    .tickPadding(10)
+  ;
 
   //formatting for output
   var formatPercent = d3.format(".1%");
@@ -41,21 +39,12 @@ export default function groupedBarChart(){
   //y axis
   var yAxis = d3.axisLeft()
     .scale(y)
-    //.orient("left")
     .tickFormat(formatPercent)
     .ticks(5)
     .tickSizeInner(-width)
     .tickSizeOuter(0)
     .tickPadding(10)
-    ;
-
-  //tool tips
-  /*var tip = d3.tip()
-    .attr('class', 'd3-tip')
-    .offset([-10, 0])
-    .html(function(d) {
-      return "<strong>Percentage:</strong> <span>" + formatPercent(d.value ) + "</span>";
-    })*/
+  ;
 
   //add svg
   var svg = d3.select("div#chartid")
@@ -67,10 +56,15 @@ export default function groupedBarChart(){
      //class to make it responsive
     .classed("svg-content-responsive padding", true);
 
-  //initialize tooltip
+    //tool tips
+  /*var tip = d3.tip()
+    .attr('class', 'd3-tip')
+    .offset([-10, 0])
+    .html(function(d) {
+      return "<strong>Percentage:</strong> <span>" + formatPercent(d.value ) + "</span>";
+    })*/
   //svg.call(tip);
 
-  //plot data 
   d3.csv("scripts/groupedBar/data.csv", function(error, data) {
     if (error) throw error;
 
@@ -83,57 +77,53 @@ export default function groupedBarChart(){
 
 
   //set domains
-  x0.domain(data.map(function(d) { return d.Issuer; }));
+  x0.domain(data.map(function(d) { return d.Issuer; }))
+  ;
+
   x1
     .domain(ageNames)
     .rangeRound([0, x0.bandwidth()])
-    ;
+  ;
   
   y.domain([0, d3.max(data, function(d) { return d3.max(d.ages, function(d) { return d.value; }); })]);
   
+  //x axis
+  svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+  //y axis
+  svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis)
+    //.append("text")
+    //.attr("transform", "rotate(-90)")
+    //.attr("y", 6)
+    //.attr("dy", ".71em")
+    //.style("text-anchor", "end")
+    //.text("percentage")
+  ;
 
+  // x axis text
+  var issuer = svg.selectAll(".issuer")
+    .data(data)
+    .enter().append("g")
+    .attr("class", "issuer")
+    .attr("transform", function(d) { return "translate(" + x0(d.Issuer) + ",0)"; })
+  ;
 
-
- 
-
-
-
-    //x axis
-    svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
-    //y axis
-    svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-      .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      //.text("percentage")
-    ;
-
-    // x axis text
-    var issuer = svg.selectAll(".issuer")
-      .data(data)
-      .enter().append("g")
-      .attr("class", "issuer")
-      .attr("transform", function(d) { return "translate(" + x0(d.Issuer) + ",0)"; });
-
-    // drawing the bars
-    issuer.selectAll("rect")
-      .data(function(d) { return d.ages; })
-      .enter().append("rect")
-      .attr("width", x1.bandwidth())
-      .attr("x", function(d) { return x1(d.name); })
-      .attr("y", function(d) { return y(d.value); })
-      .attr("height", function(d) { return height - y(d.value); })
-      .style("fill", function(d) { return color(d.name); })
-      //.on('mouseover', tip.show)
-      //.on('mouseout', tip.hide)
-      ;
+  // drawing the bars
+  issuer.selectAll("rect")
+    .data(function(d) { return d.ages; })
+    .enter().append("rect")
+    .attr("width", x1.bandwidth())
+    .attr("x", function(d) { return x1(d.name); })
+    .attr("y", function(d) { return y(d.value); })
+    .attr("height", function(d) { return height - y(d.value); })
+    .style("fill", function(d) { return color(d.name); })
+    //.on('mouseover', tip.show)
+    //.on('mouseout', tip.hide)
+  ;
 
   }); 
 }
